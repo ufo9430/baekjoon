705c900d3f04da47821d42c3bbd90ecdceed82ce
ufo9430 <ufo9430@naver.com>
2025-03-17 19:18:53 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    static long [] dp = new long[91];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i < 91; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
