d188ef2d53f27b39b767ebdeb05992c3ae0dae3d
ufo9430 <ufo9430@naver.com>
2025-04-22 18:34:50 +0900

    [level 2] Title: 올바른 괄호, Time: 9.03 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }else{
+                count--;
+            }
+            if(count < 0) return false;
+        }
+        if(count > 0) return false;
+        return true;
+    }
+}
